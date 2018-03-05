2d36c250301ca699e32ac2bae067f02f76d40962
gisgraphy <davidmasclet@gisgraphy.com>
2018-03-05 12:49:41 +0100

    don't compute the country name on client side since it is now on server side.

--- src/test/java/com/graphhopper/converter/api/GisgraphyAddressEntryTest.java
+++ src/test/java/com/graphhopper/converter/api/GisgraphyAddressEntryTest.java
@@ -3,8 +3,6 @@ package com.graphhopper.converter.api;
 import org.junit.Assert;
 import org.junit.Test;
 
-import com.graphhopper.converter.data.CountryInfo;
-
 public class GisgraphyAddressEntryTest {
 
 	@Test
@@ -26,32 +24,6 @@ public class GisgraphyAddressEntryTest {
 		Assert.assertEquals(gisgraphyAddressEntry.getName(),
 				gisgraphyAddressEntry.getDisplayName());
 
-	}
-
-	@Test
-	public void getCountry() {
-		GisgraphyAddressEntry gisgraphyAddressEntry = new GisgraphyAddressEntry();
-		gisgraphyAddressEntry.setCountryCode("DE");
-		Assert.assertEquals(CountryInfo.countryLookupMap.get("DE"),
-				gisgraphyAddressEntry.getCountry());
-
-		gisgraphyAddressEntry.setCountryCode("de");
-		Assert.assertEquals("lower case should be managed",
-				CountryInfo.countryLookupMap.get("DE"),
-				gisgraphyAddressEntry.getCountry());
-		Assert.assertNotNull(gisgraphyAddressEntry.getCountry());
-
-		gisgraphyAddressEntry.setCountryCode("");
-		Assert.assertEquals("empty code should be managed",
-				CountryInfo.countryLookupMap.get(""),
-				gisgraphyAddressEntry.getCountry());
-		Assert.assertNull(gisgraphyAddressEntry.getCountry());
-
-		gisgraphyAddressEntry.setCountryCode(null);
-		Assert.assertEquals("null code should be managed",
-				CountryInfo.countryLookupMap.get("de"),
-				gisgraphyAddressEntry.getCountry());
-		Assert.assertNull(gisgraphyAddressEntry.getCountry());
 	}
 
 }
